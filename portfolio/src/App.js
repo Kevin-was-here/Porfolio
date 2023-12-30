26e4713fe02b6d6d2485955613fcb0d35c75aa4a
kevin-was-here <code --wait>
2023-12-29 19:37:46 -0700

    file setup + templating

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
+
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +23,8 @@ function App() {
         </a>
       </header>
     </div>
+
+
   );
 }
 
