93ab6be49de34287feb42dffd35b9d91224774c0
kevin-was-here <code --wait>
2023-12-30 04:23:23 -0700

    set up folders for projects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,31 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import Blog from './pages/Blog'
+import './App.scss'
 
 function App() {
   return (
-
-
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-
-
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/blog" element={<Blog />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
