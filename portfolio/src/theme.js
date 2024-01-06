28cfa086be49565188df271e7441d883d349a84b
kevin-was-here <code --wait>
2024-01-06 10:16:52 -0700

    Added theme.js

---
+++ portfolio/src/theme.js
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: '#ff0000',
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+  typography: {
+    // Define typography styles here
+  },
+  // You can also add other theme customizations here
+});
+
+export default theme;
